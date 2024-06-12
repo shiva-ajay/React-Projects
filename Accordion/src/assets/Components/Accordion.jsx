6932281b0632958106c33704edcc7cda7be01b5f
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-12 23:43:41 +0530

    Single Selection

---
+++ Accordion/src/assets/Components/Accordion.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import data from "./data";
+
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={() => {
+                  handleSingleSelection(dataItem.id);
+                }}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ Accordion/src/assets/Components/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer: "Accordion components are user interface elements used for organizing content into collapsible sections."
+    },
+    {
+        id: '2',
+        question: "What are they used for?",
+        answer: "They are commonly employed in various contexts, including FAQs, dashboards, and content-heavy pages to improve usability and navigation."
+    },
+    {
+        id: '3',
+        question: "Accordion as a musical instrument",
+        answer: "The accordion is a musical instrument with a keyboard and bellows, used in various musical genres worldwide."
+    },
+    {
+        id: '4',
+        question: "Can I create an accordion component with a different framework?",
+        answer: "Yes, of course, it is very possible to create an accordion component with different frameworks like React, Angular, or Vue.js."
+    }
+];
+
+export default data;
